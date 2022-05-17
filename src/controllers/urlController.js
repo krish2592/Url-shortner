c86df2664f63934dc5f29521f0cf63e39ef57268
krish2592 <sachink9225s@gmail.com>
2022-05-17 23:50:59 +0530

    upated urlController

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,42 +1,59 @@
-const { nanoid } = require('nanoid')
+const { nanoid } = require('nanoid');
 const urlModel = require("../models/urlModel");
 const { isValid } = require('../utilities/validator');
 
 const createShortUrl = async function (req, res) {
-    let longUrl = req.body.longUrl;
-    if(!isValid(longUrl)) return res.status(400).send({status:false, message:"Url is required"})
-    let urlCode = longUrl.slice(1, 3)+nanoid();
-    let shortUrl = "localhost:3000/" + urlCode;
-    let saveData = { longUrl, shortUrl, urlCode }
-    let saveUrl = await urlModel.create(saveData)
-    let result = {
-        longUrl: saveUrl.longUrl,
-        shortUrl: saveUrl.shortUrl,
-        urlCode: saveUrl.urlCode
+    try {
+
+        //Getting original url from user
+        let longUrl = req.body.longUrl;
+
+        //Validating Original Url
+        if (!isValid(longUrl)) return res.status(400).send({ status: false, message: "Url is required" })
+
+        //Generating unique url code
+        let urlCode = longUrl.slice(1, 3) + nanoid();
+
+        //Checking uniqueness of url in database
+        let isUniqueUrlCode = await urlModel.findOne({ urlCode: urlCode })
+        if (isUniqueUrlCode) return res.status(400).send({ status: false, message: `${urlCode} is already exist` })
+      
+        //Generating short url
+        let shortUrl = "localhost:3000/" + urlCode;
+
+        //Saving data in database
+        let saveData = { longUrl, shortUrl, urlCode }
+        let saveUrl = await urlModel.create(saveData)
+
+        let result = {
+            longUrl: saveUrl.longUrl,
+            shortUrl: saveUrl.shortUrl,
+            urlCode: saveUrl.urlCode
+        }
+        return res.status(200).send({ data: result })
     }
-    return res.status(200).send({ data: result })
+    catch (err) {
+        res.status(500).send({ msg: "Internal Server Error", error: err.message });
+    }
 }
 
 
 const redirect2LongUrl = async function (req, res) {
     try {
 
-      let  urlCode = req.params.urlCode;
+        let urlCode = req.params.urlCode;
 
-      // if url (shortUrl) is not entered
-      if(!isValid(urlCode)) return res.status(400).send({status:false, message:"Url code is required"})
-      
-      let getData = await urlModel.findOne({ urlCode: urlCode });
-  
-      // if Url does not exist (in our database)
-      if (!getData) {
-        return res.status(404).send({ status: false, message: "Url not found" });
-      }
-  
-      res.redirect(302, getData.longUrl);
+        let getData = await urlModel.findOne({ urlCode: urlCode });
+
+        // if Url does not exist (in our database)
+        if (!getData) {
+            return res.status(404).send({ status: false, message: "Url not found" });
+        }
+
+        res.redirect(302, getData.longUrl);
     } catch (err) {
-      res.status(500).send({ msg: "Internal Server Error", error: err.message });
+        res.status(500).send({ msg: "Internal Server Error", error: err.message });
     }
-  };
+};
 
 module.exports = { createShortUrl, redirect2LongUrl }
